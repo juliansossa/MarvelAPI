ad1e9a17a5a9da83fe436455d1f68bfbbaf4501c
Julian Sossa <juliansossa6@gmail.com>
2022-05-23 22:41:18 -0500

    Primer elemento de la API mostrado en HTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,24 @@
 const obtenerDatos = async () => {
-    // 198393e811f89d60ade9fed45f8cfbb00fb1f6d67a06e5ed27894cbe75a50b6b7c57e31ed
-
-    // const datos = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a06e5ed27894cbe75a50b6b7c57e31ed&hash=6640616133ca308fe2d8bed822f9cd67");
-    // console.log(datos);
     try {
         const respuesta = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a06e5ed27894cbe75a50b6b7c57e31ed&hash=6640616133ca308fe2d8bed822f9cd67");
-        console.log(respuesta);
 
         const datos = await respuesta.json();
-        console.log(datos.data.results[0].name);
+
+        const resultados = datos.data.results;
+        console.log(resultados);
+
+        const name = resultados.map(x=>x.name);
+        console.log(name);
+        mostrarDatos(name);
+
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 obtenerDatos();
+
+const mostrarDatos = async (array) => {
+    console.log(array);
+    const Inner = document.getElementById("parrafo");
+    Inner.innerText = "name: " + array[0]
+}
