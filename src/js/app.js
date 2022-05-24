3e0f57f51b3c4835781b6d117b845dee3399f92d
Julian Sossa <juliansossa6@gmail.com>
2022-05-24 00:18:01 -0500

    Peticion devuelve nombres e imagenes de los personajes, luego se muestra en HTML esa información

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,19 @@
 const obtenerDatos = async () => {
     try {
         const respuesta = await fetch("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a06e5ed27894cbe75a50b6b7c57e31ed&hash=6640616133ca308fe2d8bed822f9cd67");
-
-        const datos = await respuesta.json();
-
-        const resultados = datos.data.results;
-        console.log(resultados);
-
-        const name = resultados.map(x=>x.name);
-        console.log(name);
-        mostrarDatos(name);
+        console.log(respuesta);
+        
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            const resultados = datos.data.results;
+            mostrarDatos(resultados);
+        }
+        else if (respuesta.status === 401) {
+            console.error("Referencia no valida, error: " + respuesta.status)
+        }
+        else if (respuesta.status === 404) {
+            console.error("La URL a la que está intentando acceder no existe, error: " + respuesta.status)
+        }
 
     } catch (error) {
         console.log(error);
@@ -18,7 +22,13 @@ const obtenerDatos = async () => {
 obtenerDatos();
 
 const mostrarDatos = async (array) => {
-    console.log(array);
-    const Inner = document.getElementById("parrafo");
-    Inner.innerText = "name: " + array[0]
+    let personajes = "";
+    array.forEach(x=>{
+        personajes += `
+        <img src="${x.thumbnail.path}.${x.thumbnail.extension}"></img>
+        <h3>${x.name}</h3>
+        `
+    });
+
+    document.getElementById("container").innerHTML = personajes;
 }
